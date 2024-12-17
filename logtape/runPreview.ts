d9863d8a37d052106492adecc80605ae5c4f020d
DmitryL <lelekodmitry@gmail.com>
2024-12-17 16:43:12 +0300

    Add preview

---
+++ logtape/runPreview.ts
@@ -0,0 +1,30 @@
+import { configure } from "./config.ts";
+import { getLogger } from "./logger.ts";
+import { getConsoleSink } from "./sink.ts";
+
+await configure({
+  sinks: {
+    console: getConsoleSink(),
+  },
+  loggers: [
+    {
+      category: "app",
+      sinks: ["console"],
+    },
+  ],
+});
+
+const ctx = {
+  str: "lorem ipsum",
+  num: 123,
+  obj: { foo: "bar", obj: { foo: "bar" } },
+  arr: [1, 2, 3],
+  fn: () => "foo",
+};
+
+const logger = getLogger("app");
+logger.debug("debug", ctx);
+logger.info("info", ctx);
+logger.warn("warn", ctx);
+logger.error("error", ctx);
+logger.fatal("fatal", ctx);
